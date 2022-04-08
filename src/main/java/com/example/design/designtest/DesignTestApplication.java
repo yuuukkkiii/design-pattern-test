f97f8c315760f08bdba62e8ecd8574278430cda0
zhaih7 <zhaih7@chinaunicom>
2022-04-08 15:37:57 +0800

    初始化项目

---
+++ src/main/java/com/example/design/designtest/DesignTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.design.designtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignTestApplication.class, args);
+    }
+
+}
